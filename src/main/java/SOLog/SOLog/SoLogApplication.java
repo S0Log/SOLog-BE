86fc59c44654aaa208cb9d1f0ffa8b9985ce75e3
kwonjieon <holemart@naver.com>
2024-11-14 13:44:57 +0900

    SOLog 프로젝트 생성

---
+++ src/main/java/SOLog/SOLog/SoLogApplication.java
@@ -0,0 +1,13 @@
+package SOLog.SOLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SOLog/SOLog/SoLogApplicationTests.java
@@ -0,0 +1,13 @@
+package SOLog.SOLog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
