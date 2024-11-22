7dc8c64e91e8d96d9fd4ff6fc9d85cf8e7b6cb53
kwonjieon <holemart@naver.com>
2024-11-22 13:10:35 +0900

    fix: 용어 정의 길이 수정, financial엔티티 속성추가

--- src/main/java/SOLog/SOLog/domain/entity/GlossaryEntity.java
+++ src/main/java/SOLog/SOLog/domain/entity/GlossaryEntity.java
@@ -1,10 +1,7 @@
 package SOLog.SOLog.domain.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,5 +17,6 @@ public class GlossaryEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String term;
+    @Column(length = 1000)
     private String definition;
 }
