3219fd67edc32f5718eb36de53d3ea8a36712586
alstjs37 <kubernetes.io@kakao.com>
2024-11-25 15:19:52 +0900

    feat: companyInfo-salesTrendRatio API 구현

--- src/main/java/SOLog/SOLog/service/CompanyInfoService.java
+++ src/main/java/SOLog/SOLog/service/CompanyInfoService.java
@@ -2,11 +2,16 @@ package SOLog.SOLog.service;
 
 import SOLog.SOLog.domain.dto.CompanyOverviewDto;
 import SOLog.SOLog.domain.dto.MarketShareDto;
+import SOLog.SOLog.domain.dto.SalesTrendRatioDto;
 import SOLog.SOLog.domain.entity.CompanyEntity;
 import SOLog.SOLog.domain.entity.MarketShareEntity;
+import SOLog.SOLog.domain.entity.SalesTrendRatioEntity;
 import SOLog.SOLog.repository.CompanyInfoRepository;
 import SOLog.SOLog.repository.MarketShareRepository;
+import SOLog.SOLog.repository.SalesTrendRatioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,6 +22,7 @@ import java.util.stream.Collectors;
 public class CompanyInfoService {
     private final CompanyInfoRepository companyInfoRepository;
     private final MarketShareRepository marketShareRepository;
+    private final SalesTrendRatioRepository salesTrendRatioRepository;
 
     // 기업개요
     public CompanyOverviewDto getCompanyOverview(String companyName) {
@@ -32,6 +38,16 @@ public class CompanyInfoService {
 
         return marketShare.stream()
                 .map(entity -> new MarketShareDto(entity.getMainProduct(), entity.getSharePercent()))
+                .collect(Collectors.toList());
+    }
+
+    // 매출비중추이
+    public List<SalesTrendRatioDto> getSalesTrendRatio(String companyName) {
+        Pageable pageable = PageRequest.of(0, 20);
+        List<SalesTrendRatioEntity> salesTrendRatio = salesTrendRatioRepository.findTop5ByCompanyName(companyName, pageable).getContent();
+
+        return salesTrendRatio.stream()
+                .map(entity -> new SalesTrendRatioDto(entity.getDate(), entity.getProductName(), entity.getSalesPercent()))
                 .collect(Collectors.toList());
     }
 }
