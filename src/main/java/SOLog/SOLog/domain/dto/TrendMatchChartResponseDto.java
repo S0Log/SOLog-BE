89c29a3305ea79a1b0f9a2c82d0204a29d952c5d
kwonjieon <holemart@naver.com>
2024-12-03 10:51:16 +0900

    feat: trendMach dto수정

--- src/main/java/SOLog/SOLog/domain/dto/TrendMatchChartResponseDto.java
+++ src/main/java/SOLog/SOLog/domain/dto/TrendMatchChartResponseDto.java
@@ -2,14 +2,17 @@ package SOLog.SOLog.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
 @AllArgsConstructor
 public class TrendMatchChartResponseDto {
-    private List<ChartDataResponseDto>[] chartDataResponseDto;
-    private Date markingDate;
+    private List<ChartDataResponseDto> baseData;//그 기간 baseData
+    private ArrayList<List<ChartDataResponseDto>> similarDataList;// 비슷한거 여러개 차트
+    private List<Date> markingDateList;
     private Date baseDate;
     private Long highlightNum;
 
